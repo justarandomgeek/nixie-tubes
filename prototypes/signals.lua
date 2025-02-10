d1c8c7da42b56956f84f11b449c3eb6ef3b34a30
justarandomgeek <justarandomgeek@gmail.com>
2025-02-09 21:44:49 -0500

    unify virtual signals with the ones added to base in 2.0.34

--- prototypes/signals.lua
+++ prototypes/signals.lua
@@ -24,26 +24,10 @@ local function create_symsignal(name,sort)
   symcount = symcount + 1
 end
 
---create_symsignal("negative")
-
 --extended symbols
-create_symsignal("paropen",'a-aa')
-create_symsignal("parclose",'a-ab')
-create_symsignal("sqopen",'a-ba')
-create_symsignal("sqclose",'a-bb')
 create_symsignal("curopen",'a-ca')
 create_symsignal("curclose",'a-cb')
-
-create_symsignal("stop",'b')
-create_symsignal("qmark",'b')
-create_symsignal("exmark",'b')
 create_symsignal("at",'c')
-
-create_symsignal("slash",'d')
-create_symsignal("asterisk",'d')
-create_symsignal("minus",'d')
-create_symsignal("plus",'d')
-create_symsignal("percent", 'd')
 
 create_symsignal("float",'x')
 create_symsignal("hex",'x')
