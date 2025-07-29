5d877d746a43849ff75d65da11e754a3b0061763
justarandomgeek <justarandomgeek@gmail.com>
2025-07-28 22:32:22 -0400

    migrate white to default color too

--- migrations/2_0_4_fix_entity_colors.lua
+++ migrations/2_0_4_fix_entity_colors.lua
@@ -2,8 +2,15 @@ for _,surf in pairs(game.surfaces) do
   -- re-index all nixies
   for _,lamp in pairs(surf.find_entities_filtered{name={'nixie-tube', 'nixie-tube-alpha', 'nixie-tube-small'}}) do
     local color = lamp.color
-    if color and color.a == 0 then
-      color.a = 1
+    if color then
+      if color.a == 0 then
+        color.a = 1
+      end
+
+      if color.r==1 and color.g==1 and color.b==1 then
+        color = {r=1, g=.6, b=.3, a=1}
+      end
+
       lamp.color = color
     end
   end
